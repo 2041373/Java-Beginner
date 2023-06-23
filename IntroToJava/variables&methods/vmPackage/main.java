cc384ca32b258f09bf482d4b2873e476fa87e343
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-06-23 21:37:12 +0100

    file update

---
+++ IntroToJava/variables&methods/vmPackage/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		multiplication(25, 12);
+		
+		double nazar  = 2;
+		double halisha  = 13;
+	
+
+
+		System.out.println( multiplication(halisha, nazar ));
+
+		
+
+	}
+	
+
+	public static double multiplication(double a , double b) {
+		double c; 
+		c = a * b; 
+		return c; 
+	}
+	
+	
+
+}
