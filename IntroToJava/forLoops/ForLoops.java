728da8d0527c5dd8415740b5ca334043044f351d
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-06-24 10:29:09 +0100

    For Loop Practise

---
+++ IntroToJava/forLoops/ForLoops.java
@@ -0,0 +1,15 @@
+package IntroToJava.forLoops;
+
+public class ForLoops {
+
+    public static int ForLoopExample(){
+        for (int i = 0; i <10 ; i++){
+            System.out.println(i);
+        }
+        return 0;
+    }
+    public static void main (String [] args){
+        
+        ForLoopExample();
+    }
+}
