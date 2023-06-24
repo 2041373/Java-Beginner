048e447ac8952005af63b98a229ac179dac69130
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-06-24 10:38:39 +0100

    While Loop
    ForeverLoop example

---
+++ IntroToJava/whileLoops/WhileLoop.java
@@ -0,0 +1,15 @@
+package IntroToJava.whileLoops;
+
+public class WhileLoop {
+
+    public static String foreverLoop(){
+        while (true){
+            System.out.println("This is a foever loop");
+        }
+    }
+    public static void main (String [] args){
+
+        foreverLoop();
+   
+    }
+}
